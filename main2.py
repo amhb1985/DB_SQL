4ad28f44a88d6c8c54525ce8529949233f3b8e8b
amir habibi <amirhabibi219@gmail.com>
2023-12-14 23:25:53 +0100

    ok in readme text

--- main2.py
+++ main2.py
@@ -34,11 +34,21 @@ class Person(Base):
     def __repr__(self):
         return f"({self.ssn}) {self.firstname} {self.lastname} ({self.gender},{self.age})"
 
-class Thing(Base):
+
+
+
+
+
+
+
+
+# ... (vorheriger Code bleibt unverändert) ...
+
+class Things(Base):
     __tablename__ = "things"
     
     tid = Column(Integer, primary_key=True)
-    description = Column(String)  # Corrected the data type to String
+    description = Column(String)  
     owner = Column(Integer, ForeignKey("people.ssn"))
 
     def __init__(self, tid, description, owner):
@@ -49,7 +59,26 @@ class Thing(Base):
     def __repr__(self):
         return f"{self.tid} {self.description} owned by {self.owner}"
 
-# Use the correct database name ('mydb') in the connection string
+class Address(Base):
+    __tablename__ = "address"
+
+    aid = Column(Integer, primary_key=True)
+    description = Column(String)
+    street = Column(String)  # Hinzugefügt für die Straße
+    owner = Column(Integer, ForeignKey("people.ssn"))
+
+    def __init__(self, aid, description, street, owner):
+        self.aid = aid
+        self.description = description
+        self.street = street
+        self.owner = owner 
+
+    def __repr__(self):
+        return f"{self.aid} {self.description} on {self.street} owned by {self.owner}"
+
+# ... (vorheriger Code bleibt unverändert) ...
+
+# Use the correct database name for example ('mydb2') in the connection string
 engine = create_engine("postgresql://postgres:amhb@localhost:5432/mydb2", echo=True)
 
 Base.metadata.create_all(bind=engine)
@@ -78,16 +107,19 @@ session.commit()
 #print(results)
 
 #select and show only Person with last name Blue or Cold
+
 #results = session.query(Person).filter(Person.lastname == "Cold")
 #for r in results:
 #   print(r)
 
 #select and show in terminal for person Page that biger than 40
+
 #results = session.query(Person).filter(Person.age > 40)
 #for r in results:
  #print(r)
 
 #select and show in terminal for person First name is like  >> .like("%A%"")
+
 #results = session.query(Person).filter(Person.firstname.like("%An%"))
 #for l in results:
  #print(l)
@@ -98,30 +130,47 @@ session.commit()
 # print(r)
 
 
-#but before that run the code we must delet the table of DB2
-t1 = Thing(1, "Car", p1.ssn)
-#session.add(t1)
-#session.commit()
+#but before that run the code we must delet the table of DB..
+t1 = Things(1, "Car", p1.ssn)
+t2 = Things(2, "Car", p1.ssn)
+t3 = Things(2, "Laptop", p2.ssn)
+t4 = Things(4, "Car", p3.ssn)
+t5 = Things(5, "Book", p3.ssn)
 
-#add another subject to our table in DB2
-t2 = Thing(2, "Car", p1.ssn)
-t3 = Thing(2, "Laptop", p2.ssn)
-t4 = Thing(4, "Car", p3.ssn)
-t5 = Thing(5, "Book", p3.ssn)
 
 #like first t1 we must add each session again and commit them
+session.add(t1)
 session.add(t2)
 session.add(t3)
 session.add(t4)
 session.add(t5)
 session.commit()
 
-results = session.query(Thing, Person).filter(Thing.owner == Person.ssn).filter(Person.firstname == "Ana").all()
-for r in results:
-    print(r)
+#we can now see as result, if u want u can delet# after code belog.
+#results = session.query(Things, Person).filter(things.owner == Person.ssn).filter(Person.firstname == "Ana").all()
+#for r in results:
+ #   print(r)
 
-    
+#so then we can add another table for adrress
+a1 = Address(1, "Car", p1.ssn)
 
+a2 = Address(2, "sadjadsg street", p1.ssn)
+a3 = Address(2, "asdas street", p2.ssn)
+a4 = Address(4, "asdasdtte street", p3.ssn)
+a5 = Address(5, "237 street ", p3.ssn)
+
+session.add(a1)
+session.add(a2)
+session.add(a3)
+session.add(a4)
+session.add(a4)
+session.add(a5)
+
+session.commit()
+
+#results = session.query(Address, Person).filter(address.owner == Person.ssn).filter(Person.firstname == "Ana").all()
+#for d in results:
+ #   print(d)
 
 
 
