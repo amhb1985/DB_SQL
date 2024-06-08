ccb791499a3453b4c6fd132dd97143ede5ea32bf
Amir Habibi <139058609+amhb1985@users.noreply.github.com>
2024-06-08 10:04:45 +0000

    des_how_connect_DB

--- DB_UM/newtest.sql
+++ DB_UM/newtest.sql
@@ -5,10 +5,27 @@
 -- this test and text is under the learning Progrom with 
 --Dr Chuck (Dr. Charles Russell Severance)in the Coursera learning platform
 
---1. the  first is about comment with - - we can just in 1 line 
+-- ## COMMENT ## 
+-- 1. the  first is about comment with - - we can just in 1 line 
 /* 2. we can in 2ways comments >> 
 with this we can cotinue next lines till
 */
+
+-- ## TERMINAL commend ##
+/* the first we must connet to our/one Database DB 
+We can under lots of progroms /terminal  for example:
+
+
+Terminal : 
+# 1, Mac  psql (SQL Shell)
+# 2, Linux bash 
+
+Programm:
+# 1, PG Admin
+# 2, DB eaver
+
+
+
 
 
 
