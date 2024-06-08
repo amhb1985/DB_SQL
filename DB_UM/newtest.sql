4370aa310e5745c804dbaa70e316d314d5455c9e
Amir Habibi <139058609+amhb1985@users.noreply.github.com>
2024-06-08 09:55:48 +0000

    2nd_comment_in_SQL

--- DB_UM/newtest.sql
+++ DB_UM/newtest.sql
@@ -2,7 +2,17 @@
 -- this is about test sql learning in the DB --
 --  i wanna test and try to reapeat some squery in the sql 
 
--- this test and text is under the learning Progrom with Dr Chuck in the Coursera öearning platform
---the  first 
+-- this test and text is under the learning Progrom with 
+--Dr Chuck (Dr. Charles Russell Severance)in the Coursera learning platform
+
+--1. the  first is about comment with - - we can just in 1 line 
+/* 2. we can in 2ways comments >> 
+with this we can cotinue next lines till
+*/
+
+
+
+
+
 
 
