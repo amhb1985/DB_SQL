1820ea4721a5a95f6dd0701c07c3f2b39ea568c9
Amir Habibi <139058609+amhb1985@users.noreply.github.com>
2024-06-08 09:50:16 +0000

    the_first_SQL_in_git

---
+++ DB_UM/newtest.sql
@@ -0,0 +1,8 @@
+
+-- this is about test sql learning in the DB --
+--  i wanna test and try to reapeat some squery in the sql 
+
+-- this test and text is under the learning Progrom with Dr Chuck in the Coursera öearning platform
+--the  first 
+
+
