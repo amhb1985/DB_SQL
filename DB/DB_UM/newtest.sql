a43cff09074d224537f09cfb8720f5095978ef1f
Amir Habibi <139058609+amhb1985@users.noreply.github.com>
2024-06-11 11:57:20 +0000

    INNER_JOIN

--- DB/DB_UM/newtest.sql
+++ DB/DB_UM/newtest.sql
@@ -290,6 +290,10 @@ SELECT album.title, artist.name
 FROM album JOIN artist
 ON album.artist_id = artist.id;
 
+SELECT album.title, album.artist_id, artist.id, artist. name
+FROM album INNER JOIN artist 
+ON album.artist_id = artist.id;
+
 
 
 
