9dafbdf790c837948868afa8f7c2219ca1413c4c
Amir Habibi <139058609+amhb1985@users.noreply.github.com>
2024-06-19 18:57:24 +0000

    mistake_alter

--- DB/DB_UM/pg4e_2.sql
+++ DB/DB_UM/pg4e_2.sql
@@ -1,3 +1,23 @@
 -- this is the 2nd course of pg4e 
 
 -- this is about CREATE TABLE
+
+/*
+ we can adjust our schema
+
+ . Sometimes you make a mistake or your application evolves
+ 
+*/
+create table fav (
+    id serial.
+    oops text,
+    post_id integer REFERENCES post(id) on DELETE CASCADE ,
+    acount_id INTEGER REFERENCES account (id) ON DELETE CASCADE,
+    UNIQUE(post_id,acount_id),
+    PRIMARY KEY(id) 
+);
+
+ALTER TABLE fav DROP COLUMN oops;
+
+
+
